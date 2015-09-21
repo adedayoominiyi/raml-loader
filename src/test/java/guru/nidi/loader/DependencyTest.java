cfb481d0b82ca4d410daa0ae9df4538a50e7f446
Stefan Niederhauser <ghuder5@gmx.ch>
2015-09-22 01:43:16 +0200

    update jdepend

--- src/test/java/guru/nidi/loader/DependencyTest.java
+++ src/test/java/guru/nidi/loader/DependencyTest.java
@@ -15,7 +15,7 @@
  */
 package guru.nidi.loader;
 
-import jdepend.framework.DependencyConstraint;
+import jdepend.framework.DependencyDefiner;
 import jdepend.framework.JDepend;
 import jdepend.framework.JavaPackage;
 import jdepend.framework.PackageFilter;
@@ -33,18 +33,12 @@ import static org.junit.Assert.assertThat;
  *
  */
 public class DependencyTest {
-    private static final String BASE = "guru.nidi.loader";
     private static JDepend depend;
 
     @BeforeClass
     public static void init() throws IOException {
         final List<String> ramlParserPackages = Arrays.asList("org.raml.model", "org.raml.parser");
-        depend = new JDepend(new PackageFilter(Arrays.asList("org.", "java.", "com.", "javax.")) {
-            @Override
-            public boolean accept(String packageName) {
-                return ramlParserPackages.contains(packageName) || super.accept(packageName);
-            }
-        });
+        depend = new JDepend(PackageFilter.empty().excluding("org.", "java.", "com.", "javax.", "org.raml.parser.").including(ramlParserPackages));
         depend.addDirectory("target/classes");
         for (final String rpp : ramlParserPackages) {
             depend.addPackage(rpp);
@@ -54,40 +48,27 @@ public class DependencyTest {
 
     @Test
     public void dependencies() throws IOException {
-        DependencyConstraint constraint = new DependencyConstraint();
+        class OrgRaml {
+            JavaPackage model, parser;
+        }
+        final OrgRaml orgRaml = new OrgRaml();
 
-        final JavaPackage
-                apidesigner = constraint.addPackage(BASE + ".apidesigner"),
-                basic = constraint.addPackage(BASE + ".basic"),
-                repo = constraint.addPackage(BASE + ".repository"),
-                url = constraint.addPackage(BASE + ".url"),
-                jsonschema = constraint.addPackage(BASE + ".use.jsonschema"),
-                raml = constraint.addPackage(BASE + ".use.raml"),
-                xml = constraint.addPackage(BASE + ".use.xml"),
-                base = constraint.addPackage(BASE),
-                parserModel = constraint.addPackage("org.raml.model"),
-                parserParser = constraint.addPackage("org.raml.parser");
+        class GuruNidiLoader implements DependencyDefiner {
+            JavaPackage self, apidesigner, basic, repository, url, useJsonschema, useRaml, useXml;
 
-        apidesigner.dependsUpon(repo);
-        apidesigner.dependsUpon(url);
-        apidesigner.dependsUpon(base);
+            @Override
+            public void dependUpon() {
+                apidesigner.dependsUpon(repository, url, self);
+                basic.dependsUpon(self);
+                useJsonschema.dependsUpon(self);
+                useRaml.dependsUpon(orgRaml.model, self);
+                useXml.dependsUpon(self);
+                repository.dependsUpon(self);
+                url.dependsUpon(self);
+            }
+        }
 
-        basic.dependsUpon(base);
-
-        jsonschema.dependsUpon(base);
-
-        raml.dependsUpon(parserModel);
-        //jdepend is not clever enough to find this dependency
-//        raml.dependsUpon(parserParser);
-        raml.dependsUpon(base);
-
-        xml.dependsUpon(base);
-
-        repo.dependsUpon(base);
-
-        url.dependsUpon(base);
-
-        assertThat(depend, matches(constraint));
+        assertThat(depend, matchesPackages(orgRaml, new GuruNidiLoader()));
     }
 
     @Test
