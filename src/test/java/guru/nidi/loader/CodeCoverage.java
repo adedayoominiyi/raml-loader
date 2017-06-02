73ec99c6d4c6b9878bafb2357a59afd4eb682cc1
Stefan Niederhauser <ghuder5@gmx.ch>
2017-06-02 12:33:30 +0200

    update parent pom

--- src/test/java/guru/nidi/loader/CodeCoverage.java
+++ src/test/java/guru/nidi/loader/CodeCoverage.java
@@ -34,6 +34,7 @@ public class CodeCoverage {
         final JacocoAnalyzer analyzer = new JacocoAnalyzer(new CoverageCollector(BRANCH, LINE, METHOD)
                 .just(For.global().setMinima(25, 40, 40))
                 .just(For.allPackages().setMinima(40, 40, 40))
+                .just(For.packge("*.loader").setMinima(40, 40, 30))
                 .just(For.packge("*.apidesigner").setMinima(75, 10, 10))
                 .just(For.packge("*.repository").setMinima(0, 40, 60))
                 .just(For.packge("*.raml").setMinima(0, 10, 20))
